c130b9a8bd4ef3f014b034ff0e3c878bc993135d
manasadasari1 <52671174+manasadasari1@users.noreply.github.com>
2022-10-18 15:24:14 +0530

    Add files via upload
    Abstraction

---
+++ ConsoleAppBasics3/ConsoleAppBasics3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppBasics3
+{
+    abstract class Program
+    {
+        public abstract int area();
+    }
+    class Prog : Program
+    {
+        int side;
+
+
+        public Prog()
+        {
+            side = 1;
+        }
+
+
+
+        public override int area()
+        {
+            return side * side;
+        }
+    }
+    class MAins
+    {
+        public static void Main(string[] args)
+        {
+            Prog obj = new Prog();
+            Console.WriteLine(obj.area());
+        }
+    }
+}
