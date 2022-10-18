9f692b50a447fa6754a9ca87187ed764855598c4
manasadasari1 <52671174+manasadasari1@users.noreply.github.com>
2022-10-18 15:16:39 +0530

    Add files via upload

---
+++ ConsoleAppBasics1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppBasics1
+{
+
+    public class Program
+    {
+        private String name;
+        private int age;
+
+        public String Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+    }
+    class MAINS
+    {
+        public static void Main(string[] args)
+        {
+            Program bj = new Program();
+            bj.Name = "Manasa";
+            bj.Age = 22;
+            Console.WriteLine(bj.Name);
+            Console.WriteLine(bj.Age);
+        }
+    }
+}
+
