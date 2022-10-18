14d07a1bb538f7ff9b63a03238ec0c8de52da26f
manasadasari1 <52671174+manasadasari1@users.noreply.github.com>
2022-10-18 15:12:21 +0530

    Add files via upload

---
+++ ConsoleAppBasics/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppBasics
+{
+   class Program
+        {
+            public String name;
+            public int age;
+
+            public void method(String name, int age)
+            {
+                this.name = name;
+                this.age = age;
+                Console.WriteLine("name " + name + " age " + age);
+            }
+
+
+        }
+        class Program2 : Program
+        {
+            public Program2()
+            {
+                Console.WriteLine("Details");
+            }
+        }
+        class Prg3
+        {
+            public static void Main(string[] args)
+            {
+                Program2 prg = new Program2();
+                prg.method("Manasa", 21);
+            }
+        }
+    }
+
